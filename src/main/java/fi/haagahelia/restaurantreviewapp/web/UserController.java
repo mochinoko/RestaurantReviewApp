2913e8ff111d3de73368ad643e6ffb5b1698ac06
Yuki Sugiyama <35254255+mochinoko@users.noreply.github.com>
2020-11-16 18:15:28 +0200

    modified UserController and signup html css

--- src/main/java/fi/haagahelia/restaurantreviewapp/web/UserController.java
+++ src/main/java/fi/haagahelia/restaurantreviewapp/web/UserController.java
@@ -34,6 +34,7 @@ public class UserController {
 	     * @param bindingResult
 	     * @return
 	     */
+	 
 	    @RequestMapping(value = "saveuser", method = RequestMethod.POST)
 	    public String save(@Valid @ModelAttribute("signupform") SignupForm signupForm, BindingResult bindingResult) {
 	    	System.out.println("SAVESAVE");
